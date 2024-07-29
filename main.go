9a97efc709db8c2d56cc3de3315a5e44b3a61626
Aviral Asthana <aviral.asthana0704@gmail.com>
2024-07-29 23:53:05 +0530

    feat: Add greeter function to main.go

--- main.go
+++ main.go
@@ -4,4 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello to my new program")
+	fmt.Println(greeter("John"))
+}
+
+func greeter(name string) string {
+	return "Hello " + name + ". How are you?"
 }
