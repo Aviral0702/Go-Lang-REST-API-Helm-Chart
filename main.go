0e26bfd537a815b9e709f8d800329c119d6803c4
Aviral Asthana <aviral.asthana0704@gmail.com>
2024-07-30 00:05:02 +0530

    chore: Refactor main.go to improve code structure and readability

--- main.go
+++ main.go
@@ -2,31 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"net/http"
 )
 
 func main() {
 	fmt.Println("hello to my new program")
-	fmt.Println(greeter("John"))
-	info()
-}
-
-func greeter(name string) string {
-	return "Hello " + name + ". How are you?"
-
-}
-
-func info() {
-	resp, err := http.Get("http://api.weatherapi.com/v1/current.json?key=9a340ab233594c87982144702242707&q=London&aqi=no")
-	if err != nil {
-		fmt.Println("Error in fetching data")
-	}
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-	content := string(body)
-	fmt.Println(content)
-
 }
