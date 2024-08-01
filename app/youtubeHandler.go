9cd88986a7750e3da895d74bfe40d752bf762d48
Aviral Asthana <aviral.asthana0704@gmail.com>
2024-08-01 22:52:50 +0530

    chore: Update youtubeHandler.go to retrieve channel statistics

--- app/youtubeHandler.go
+++ app/youtubeHandler.go
@@ -32,8 +32,9 @@ func getChannelStats() httprouter.Handle {
 		if err != nil {
 			fmt.Println("failed to create a service")
 		}
-		yts.Channels.list([]string{"contentDetails"})
-
+		call := yts.Channels.list([]string{"contentDetails"})
+		resp, err := call.Mine(true).Do()
+		fmt.Println("Response ", resp)
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 
