6c2a68e932d0260f657c2191129f0cf8f830e471
Aviral Asthana <aviral.asthana0704@gmail.com>
2024-08-03 01:31:28 +0530

    Refactor getChannelStats function in youtubeHandler.go to retrieve channel statistics with specified channel ID

--- app/youtubeHandler.go
+++ app/youtubeHandler.go
@@ -4,11 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/julienschmidt/httprouter"
 	"google.golang.org/api/option"
 	"google.golang.org/api/youtube/v3"
@@ -21,36 +18,45 @@ type YoutubeStats struct {
 	Views          int    `json:"views"`
 }
 
-func getChannelStats(k string) httprouter.Handle {
+func getChannelStats(k string, channelId string) httprouter.Handle {
 	return httprouter.Handle(func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		// w.Write([]byte("Hello, World!"))
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatalf("Error loading .env file")
-		}
-		yt := YoutubeStats{
-			Subscribers:    50,
-			ChannelName:    "Codehakase",
-			MinutesWatched: 1000,
-			Views:          5000,
-		}
 		ctx := context.Background()
 		yts, err := youtube.NewService(ctx, option.WithAPIKey(k))
 		if err != nil {
 			fmt.Println("failed to create a service")
+			w.WriteHeader(http.StatusBadRequest)
+			return
 		}
 		call := yts.Channels.List([]string{"snippet,contentDetails,statistics"})
-		channelId := os.Getenv(CHANNEL_ID)
 		resp, err := call.Id(channelId).Do()
 		if err != nil {
 			fmt.Println("failed to fetch channel stats")
+			w.WriteHeader(http.StatusBadRequest)
+			return
 		}
-		fmt.Println("Response is here: ", resp)
+		fmt.Println(resp.Items[0].Snippet.Title)
+
+		yt := YoutubeStats{}
+		if len(resp.Items) > 0 {
+			val := resp.Items[0]
+			yt = YoutubeStats{
+				Subscribers: int(val.Statistics.SubscriberCount),
+				ChannelName: val.Snippet.Title,
+				Views:       int(val.Statistics.ViewCount),
+			}
+		} else {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 
 		if err := json.NewEncoder(w).Encode(yt); err != nil {
-			panic(err)
+			w.WriteHeader(http.StatusBadRequest)
+			// log.Fatal("error while encoding the response")
+			return
 		}
 	})
 }
